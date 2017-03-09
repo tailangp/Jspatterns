e0f0757ee5645844651d1db9f79e671b7d4985ba
Tailang <prashant-tailang@MERCER.COM>
2017-03-09 19:29:54 +0530

    Commit Testing with Karma

---
+++ tests/cocnstructor.test.js
@@ -0,0 +1,17 @@
+describe("Javascript Design Patterns Test Cases", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
